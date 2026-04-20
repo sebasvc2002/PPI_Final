66a24d8200b67ae9ed9c95cae7c4ec07a1c1c29e
Sebastian <sebastian.velasco@anahuac.mx>
2026-04-20 11:07:24 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600&family=Playfair+Display:ital,wght@0,600;1,600&display=swap" rel="stylesheet">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Bootstrap Test</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand font-playfair fs-3" href="/index.php">Las Delicias Horneadas</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/index.php">Menu</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+    <section class='hero-section'>
+        <div class="container mb-5">
+            <h1 class="hero-title font-playfair mb-4">Postres Artesanales</h1>
+            <p class="lead mb-5 fs-4">Horneados diariamente con ingredientes auténticos</p>
+            <a href="#menu" class="btn btn-accent btn-lg shadow">Explora Nuestro Menu</a>
+        </div>
+    </section>
+</html>
