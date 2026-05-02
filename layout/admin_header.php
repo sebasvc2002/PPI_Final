7baeb84c48fa82747a23ee6aa7aecb4b84a3d93d
Sebastian <sebastian.velasco@anahuac.mx>
2026-05-01 21:29:30 -0600

    started admin panel

--- layout/admin_header.php
+++ layout/admin_header.php
@@ -16,33 +16,29 @@ if (session_status() === PHP_SESSION_NONE) {
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
         <div class="container-fluid">
-            <a class="navbar-brand font-playfair fs-3" href="index.php">Las Delicias Hôrneadas</a>
+            <a class="navbar-brand font-playfair fs-3" href="index.php">Admin - Las Delicias Hôrneadas</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse align-items-end" id="navbarNav">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a href="index.php" class="nav-link">Inicio</a>
+                        <a href="index.php" class="nav-link"><i class="bi bi-speedometer2 fs-5 text-white"></i> Dashboard</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="menu.php">Menú</a>
+                        <a class="nav-link" href="menu.php"><i class="bi bi-receipt fs-5 text-white"></i> Menú</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link"><i class="bi bi-tags fs-5 text-white"></i> Categorías</a>
                     </li>
                 </ul>
                 <ul class="navbar-nav">
-                    <li class="nav-item"><a href="" class="nav-link"><i class="bi bi-cart fs-5 text-white"></i> Carrito</a></li>
-                    <li class="nav-item"><a href="cart.php" class="nav-link"></a></li>
-                    <?php if(isset($_SESSION['user_id'])): ?>
-                    <li class="nav-item"><a href="account.php" class="nav-link"><i class="bi bi-person-fill fs-5 text-white"></i>Mi Cuenta</a></li>
-                    <li class="nav-item"><a href="php/logout.php" class="nav-link"><i class="bi bi-door-closed"></i> Cerrar Sesión</a></li>
-                    <?php else: ?>
-                    <li class="nav-item"><a href="login.php" class="nav-link"><i class="bi bi-person-fill fs-5 text-white"></i> Cuenta</a></li>
-                    <?php endif; ?>
+                    <li class="nav-item"><a href="../php/logout.php" class="nav-link"><i class="bi bi-door-closed fs-5 text-white"></i> Cerrar Sesión</a></li>
                 </ul>
             </div>
         </div>
